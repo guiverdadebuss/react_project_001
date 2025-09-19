6171fbc4974729427a056c5dcb2b188f21b38371
guiverdadebuss <guilhermeverdadebuss@gmail.com>
2025-09-19 20:02:03 +0100

    feat: updating react project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-
-
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-purple-700">
