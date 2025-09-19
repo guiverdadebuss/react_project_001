abbd1bc1efe57e6885e04d5341d759a4f438f9a8
guiverdadebuss <guilhermeverdadebuss@gmail.com>
2025-09-19 14:13:52 +0100

    feat: react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-purple-700">
+      <h1 className="text-8xl font-extrabold text-white">
+        BYTES<span className="text-teal-500">4</span>FUTURE
+      </h1>
+    </div>
+  );
+}
+
+export default App;
